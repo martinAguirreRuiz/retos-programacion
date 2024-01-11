b7f47bf6b758e5641c442a6da1c1a6e35e29568a
martinAguirreRuiz <martinfaguirreruiz@gmail.com>
2024-01-11 10:38:47 -0300

    reto 09

---
+++ Retos/reto-09/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Crea un programa se encargue de transformar un número
+ * decimal a binario sin utilizar funciones propias del lenguaje que lo hagan directamente.
+ */
+
+Console.WriteLine("Ingrese un número entero por favor");
+int num = int.Parse(Console.ReadLine());
+string binario = "";
+
+while (num > 1)
+{
+    int resto = num % 2;
+    binario = resto.ToString() + binario;
+    num = num / 2;
+    if (num == 1)
+    {
+        binario = "1" + binario;
+    }
+}
+
+Console.WriteLine(binario);
